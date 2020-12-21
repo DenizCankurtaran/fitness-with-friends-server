c3305fae4a23857d7ac6ff85d75f87fe79251acb
Deniz Cankurtaran <deniz.cankurtaran@hotmail.de>
2020-12-21 13:56:07 +0100

    add friends

--- routes/user.js
+++ routes/user.js
@@ -20,6 +20,60 @@ router.get('/one/', async(req, res) => {
   }
 });
 
+router.get('/search/', async (req, res) => {
+  let { username } = req.body.query;
+  let [err, users] = await UserService.findUsers({username});
+  if (err) {
+    res.status(500);
+    res.json({
+        status: false,
+        error: err.message,
+        stack: process.env.NODE_ENV === 'production' ? '' : err.stack
+    });
+  } else {
+    res.json({
+      status: true,
+      users //man schickt passwort mit lol
+    });
+  }
+});
+
+router.put('/add/', async (req, res) => {
+  let { _id } = req.body.user;
+  let friendId = req.body.friend._id; // UwU
+  let [err, user] = await UserService.findUserById(_id);
+  if (err) {
+    res.status(500);
+    res.json({
+        status: false,
+        error: err.message,
+        stack: process.env.NODE_ENV === 'production' ? '' : err.stack
+    });
+  } else {
+    const [err, friend] = await UserService.findUserById(friendId);
+    if (err) {
+      res.status(500);
+      res.json({
+          status: false,
+          error: err.message,
+          stack: process.env.NODE_ENV === 'production' ? '' : err.stack
+      });
+    } else {
+      let index = user.friends.indexOf(friendId);
+      if (index !== -1) {
+        user.friends.splice(index, 1);
+      } else {
+        user.friends.push(friendId);
+      }
+      user.save();
+      res.json({
+        status: true
+      });
+    }
+  }
+
+});
+
 router.put('/update/', async (req, res) => {
   let {user, upUser, update} = req.body;
   if (user._id === upUser._id ) {
@@ -30,7 +84,7 @@ router.put('/update/', async (req, res) => {
               status: false,
               error: err.message,
               stack: process.env.NODE_ENV === 'production' ? '' : err.stack
-          })
+          });
       } else {
           res.status(202);
           res.json({
@@ -78,4 +132,3 @@ router.delete('/delete/', async (req, res) => {
 });
 
 module.exports = router;
-
